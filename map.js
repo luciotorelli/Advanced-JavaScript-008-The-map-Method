ef8dadb0087af4d70f7ad69a4fefb6ae4650f348
luciotorelli <luciotorelli@hotmail.com>
2023-05-30 14:30:12 +0100

    Finish exercise

--- map.js
+++ map.js
@@ -5,16 +5,30 @@
  */
 
 // Using a for loop
+let nums = [1, 2, 3, 4, 5];
+let results = [];
 
+for(let num of nums) {
+  results.push(num * 2);
+}
+
+console.log(results);
 
 // Using map()
+const multByTwo = function (num) {
+  return num * 2;
+}
 
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
 
 // Simplified w/ map()
+const simplified = nums.map(function (num) { return num * 2 });
+console.log(simplified);
 
-
-// Simplfied w/ map() + arrow function
-
+// Simplified w/ map() + arrow function
+const simplifiedWithArrow = nums.map(num => num * 2);
+console.log(simplifiedWithArrow);
 
 // With objects:
 const students = [
@@ -37,3 +51,6 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+let studentsIdsAndNames = students.map(student => [student.name, student.id]);
+console.log(studentsIdsAndNames);
